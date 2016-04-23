87f2890586bb82fe0e023af94c6de36f80aafc03
Razaik Boparai <razaikb@gmail.com>
2016-04-23 15:55:49 -0400

    sets up boilerplate for redux booklist project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// book reducer which is a function that returns a piece of our application state which is a list of books
+function() {
+  return [
+    {title: 'JavaScript' },
+    {title: 'Harry Potter'},
+    {title: 'The Dark Knight'},
+    {title: 'Lego Movie'}
+  ]
+}
