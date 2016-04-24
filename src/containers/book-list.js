c4be7f60e6c7bfbaeac7512df8e167bc7f6b0ef8
Razaik Boparai <razaikb@gmail.com>
2016-04-23 23:57:37 -0400

    updates book-list with connect function from react-redux library

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,14 @@
 // to render a list of books
 // using react-redux library to connect react with redux state
 // react-redux forms the bridge between react and redux
-// so we promote this Component to a container so that it will have a knowledge of redux state 
+// so we promote this Component to a container so that it will have a knowledge of redux state
 
+
+// if we use curly braces we pull off a sigle property
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 
   // helper function
   renderList() {
@@ -23,5 +26,16 @@ export default class BookList extends Component {
      </ul>
    )
   }
+
+}
+// connect from react-redux provides mapStateToProps function
+function mapStateToProps(state) {
+  // whatever is returned will show up as props
+  // inside of BookList
+  return (
+
+
+  );
+
 
 }
